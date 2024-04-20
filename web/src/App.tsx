0806318b1f72f55f8b43650a8476dd1a40275e93
Titouan Petit <petit.titouan1@gmail.com>
2024-04-20 23:11:04 +0200

    Installed Mantine
    & Changed App Title Formatting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,23 @@
-import {FatalErrorBoundary, RedwoodProvider} from '@redwoodjs/web'
-import {RedwoodApolloProvider} from '@redwoodjs/web/apollo'
+import { MantineProvider } from '@mantine/core'
 
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import '@mantine/core/styles.css'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import theme from '../config/mantine.config'
 import './index.css'
 
 const App = () => (
     <FatalErrorBoundary page={FatalErrorPage}>
-        <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-            <RedwoodApolloProvider>
-                <Routes/>
-            </RedwoodApolloProvider>
+        <RedwoodProvider titleTemplate="%PageTitle - %AppTitle">
+            <MantineProvider theme={theme} defaultColorScheme={'auto'}>
+                <RedwoodApolloProvider>
+                    <Routes />
+                </RedwoodApolloProvider>
+            </MantineProvider>
         </RedwoodProvider>
     </FatalErrorBoundary>
 )
